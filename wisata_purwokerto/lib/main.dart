cff9f6fd9b18dbb06b66e956a3c0518bfbc15205
Tiofani99 <tiofani03@gmail.com>
2020-06-24 21:27:12 +0700

    initial commit

---
+++ wisata_purwokerto/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Purwokerto',
+      theme: ThemeData.dark(),
+      home: DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
