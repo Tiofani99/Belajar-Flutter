b2ed604995376e3491c83eaeb02970a73fb4e9d5
Tiofani99 <tiofani03@gmail.com>
2020-06-24 20:58:05 +0700

    menambahkan menu sidebar

--- scaffold/lib/main.dart
+++ scaffold/lib/main.dart
@@ -20,6 +20,9 @@ class FirstScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu,color: Colors.white,),
+        ),
         title: Text('First Screen'),
         actions: <Widget>[
           IconButton(
