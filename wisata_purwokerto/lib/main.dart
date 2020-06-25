eb5f23eb3946538cd9d7f3947b0e267706032a79
Tiofani99 <tiofani03@gmail.com>
2020-06-25 10:30:26 +0700

    memindahkan widget detail ke kelas detail_screen.dart

--- wisata_purwokerto/lib/main.dart
+++ wisata_purwokerto/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,69 +15,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 16.0),
-              child: Text(
-                  'Lokawisata baturaden',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-
-              )),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.calendar_today),
-                    SizedBox(height: 8.0,),
-                    Text('Open Everyday'),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.access_time),
-                    SizedBox(height: 8.0),
-                    Text('09.00 - 20.00'),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.monetization_on),
-                    SizedBox(height: 8.0,),
-                    Text('Rp 17.000')
-
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Berada di kaki Gunung Slamet, Baturaden menjadi destinasi wisata keluarga yang tidak membosankan'
-                  'karena lingkungan alamnya yang indah dan asri, sehingga membuat liburan anda terasa natural dan segar.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16.0),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
 
 
 
