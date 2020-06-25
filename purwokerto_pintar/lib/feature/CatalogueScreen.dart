3ae4a1b712c794a52bbd4eb0daf4f0bd0f11b180
Tiofani99 <tiofani03@gmail.com>
2020-06-25 22:56:27 +0700

    menambahkan data baru

--- purwokerto_pintar/lib/feature/CatalogueScreen.dart
+++ purwokerto_pintar/lib/feature/CatalogueScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:purwokertopintar/model/Category.dart';
+import 'package:purwokertopintar/model/Lokawisata.dart';
 
 class CatalogueScreen extends StatelessWidget{
   final Category category;
@@ -11,8 +12,61 @@ class CatalogueScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Colors.black,
+
     );
   }
 
 }
+
+var listLokawisata = [
+  Lokawisata(
+    name: "Baturaden",
+    location: "Lokawisata, Dusun I Karangmangu, Karangmangu, Kec.Baturaden, Kabupaten Banyumas, Jawa Tengah, 53151.",
+    description: "Baturaden merupakan destinasi wisata yang cocok dikunjungi bersama keluarga, karena di dalamnya terdapat berbagai fasilitas hiburan yang dapat dinikmati anak-anak hingga dewasa. Lokasinya yang berada di kaki Gunung Slamet membuat hawa di Lokawisata Baturaden terasa sejuk.",
+    openDays: "Setiap hari" ,
+    openTime: "08.00 - 17.00",
+    ticketPrice: "Rp 14.000",
+    imageAsset: "https://firebasestorage.googleapis.com/v0/b/jalan-yuk.appspot.com/o/g_baturaden.jpg?alt=media&token=abfb57c8-5935-41d5-8bef-0fbf04a55714",
+  ),
+
+  Lokawisata(
+    name: "Baturaden",
+    location: "Lokawisata, Dusun I Karangmangu, Karangmangu, Kec.Baturaden, Kabupaten Banyumas, Jawa Tengah, 53151.",
+    description: "Baturaden merupakan destinasi wisata yang cocok dikunjungi bersama keluarga, karena di dalamnya terdapat berbagai fasilitas hiburan yang dapat dinikmati anak-anak hingga dewasa. Lokasinya yang berada di kaki Gunung Slamet membuat hawa di Lokawisata Baturaden terasa sejuk.",
+    openDays: "Setiap hari" ,
+    openTime: "08.00 - 17.00",
+    ticketPrice: "Rp 14.000",
+    imageAsset: "https://firebasestorage.googleapis.com/v0/b/jalan-yuk.appspot.com/o/g_baturaden.jpg?alt=media&token=abfb57c8-5935-41d5-8bef-0fbf04a55714",
+  ),
+
+  Lokawisata(
+    name: "Baturaden",
+    location: "Lokawisata, Dusun I Karangmangu, Karangmangu, Kec.Baturaden, Kabupaten Banyumas, Jawa Tengah, 53151.",
+    description: "Baturaden merupakan destinasi wisata yang cocok dikunjungi bersama keluarga, karena di dalamnya terdapat berbagai fasilitas hiburan yang dapat dinikmati anak-anak hingga dewasa. Lokasinya yang berada di kaki Gunung Slamet membuat hawa di Lokawisata Baturaden terasa sejuk.",
+    openDays: "Setiap hari" ,
+    openTime: "08.00 - 17.00",
+    ticketPrice: "Rp 14.000",
+    imageAsset: "https://firebasestorage.googleapis.com/v0/b/jalan-yuk.appspot.com/o/g_baturaden.jpg?alt=media&token=abfb57c8-5935-41d5-8bef-0fbf04a55714",
+  ),
+
+  Lokawisata(
+    name: "Baturaden",
+    location: "Lokawisata, Dusun I Karangmangu, Karangmangu, Kec.Baturaden, Kabupaten Banyumas, Jawa Tengah, 53151.",
+    description: "Baturaden merupakan destinasi wisata yang cocok dikunjungi bersama keluarga, karena di dalamnya terdapat berbagai fasilitas hiburan yang dapat dinikmati anak-anak hingga dewasa. Lokasinya yang berada di kaki Gunung Slamet membuat hawa di Lokawisata Baturaden terasa sejuk.",
+    openDays: "Setiap hari" ,
+    openTime: "08.00 - 17.00",
+    ticketPrice: "Rp 14.000",
+    imageAsset: "https://firebasestorage.googleapis.com/v0/b/jalan-yuk.appspot.com/o/g_baturaden.jpg?alt=media&token=abfb57c8-5935-41d5-8bef-0fbf04a55714",
+  ),
+
+  Lokawisata(
+    name: "Baturaden",
+    location: "Lokawisata, Dusun I Karangmangu, Karangmangu, Kec.Baturaden, Kabupaten Banyumas, Jawa Tengah, 53151.",
+    description: "Baturaden merupakan destinasi wisata yang cocok dikunjungi bersama keluarga, karena di dalamnya terdapat berbagai fasilitas hiburan yang dapat dinikmati anak-anak hingga dewasa. Lokasinya yang berada di kaki Gunung Slamet membuat hawa di Lokawisata Baturaden terasa sejuk.",
+    openDays: "Setiap hari" ,
+    openTime: "08.00 - 17.00",
+    ticketPrice: "Rp 14.000",
+    imageAsset: "https://firebasestorage.googleapis.com/v0/b/jalan-yuk.appspot.com/o/g_baturaden.jpg?alt=media&token=abfb57c8-5935-41d5-8bef-0fbf04a55714",
+  ),
+
+];
